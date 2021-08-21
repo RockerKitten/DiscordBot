7d2772f1ebc479dd1231c66ec5f3f09bd4615f11
RockerKitten <84648053+RockerKitten@users.noreply.github.com>
2021-08-21 00:46:13 -0500

    Initial push

---
+++ DiscordBot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+  internal class Program
+  {
+    public static void Main(string[] args)
+    => new Program().MainAsync().GetAwaiter().GetResult();
+
+    public async Task MainAsync()
+    {
+      
+    }
+    private Task Log(LogMessage msg)
+    {
+      Console.WriteLine(msg.ToString());
+      return Task.CompletedTask;
+    }
+  }
+}
